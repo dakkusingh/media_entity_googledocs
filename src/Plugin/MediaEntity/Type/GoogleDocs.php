7d52e04e091e425b302404ffa640d0549e2992f0
dakku <dakku@97634.no-reply.drupal.org>
2017-04-19 12:11:40 +0100

    First WIP commit of media entity - lots to cleanup

---
+++ src/Plugin/MediaEntity/Type/GoogleDocs.php
@@ -0,0 +1,215 @@
+<?php
+
+namespace Drupal\media_entity_googledocs\Plugin\MediaEntity\Type;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityFieldManagerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\media_entity\MediaInterface;
+use Drupal\media_entity\MediaTypeBase;
+use Drupal\media_entity\MediaTypeException;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides media type plugin for GoogleDocs.
+ *
+ * @MediaType(
+ *   id = "googledocs",
+ *   label = @Translation("GoogleDocs"),
+ *   description = @Translation("Provides business logic and metadata for GoogleDocs.")
+ * )
+ */
+class GoogleDocs extends MediaTypeBase {
+
+  /**
+   * Config factory service.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructs a new class instance.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   Entity type manager service.
+   * @param \Drupal\Core\Entity\EntityFieldManagerInterface $entity_field_manager
+   *   Entity field manager service.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   Config factory service.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, EntityFieldManagerInterface $entity_field_manager, ConfigFactoryInterface $config_factory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $entity_field_manager, $config_factory->get('media_entity.settings'));
+    $this->configFactory = $config_factory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('entity_field.manager'),
+      $container->get('config.factory')
+    );
+  }
+
+  /**
+   * List of validation regular expressions.
+   *
+   * @var array
+   */
+  public static $validationRegexp = array(
+    // TODO Allow embedding bu URL.
+    // '@((http|https):){0,1}//(www\.){0,1}googledocs\.com/gallery/(?<shortcode>[a-z0-9_-]+)@i' => 'shortcode',
+    '@(?P<shortcode><blockquote class=\"googledocs-embed-pub\" lang=\"en\" data-id=\"(.*)\"><a href=\"//googledocs.com/(?P<googledocsid>.*)\">(.*)</a></blockquote><script async src=\"//s.googledocs.com/min/embed.js\" charset=\"utf-8\"></script>)@i' => 'shortcode',
+  );
+
+  /**
+   * {@inheritdoc}
+   */
+  public function providedFields() {
+    $fields = array(
+      'shortcode' => $this->t('GoogleDocs shortcode'),
+    );
+
+    return $fields;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getField(MediaInterface $media, $name) {
+    $matches = $this->matchRegexp($media);
+
+    if (!$matches['shortcode']) {
+      return FALSE;
+    }
+
+    if ($name == 'shortcode') {
+      return $matches['shortcode'];
+    }
+
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $options = [];
+    $bundle = $form_state->getFormObject()->getEntity();
+    $allowed_field_types = ['string', 'string_long', 'link'];
+    foreach ($this->entityFieldManager->getFieldDefinitions('media', $bundle->id()) as $field_name => $field) {
+      if (in_array($field->getType(), $allowed_field_types) && !$field->getFieldStorageDefinition()->isBaseField()) {
+        $options[$field_name] = $field->getLabel();
+      }
+    }
+
+    $form['source_field'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Field with source information'),
+      '#description' => $this->t('Field on media entity that stores GoogleDocs embed code or URL. You can create a bundle without selecting a value for this dropdown initially. This dropdown can be populated after adding fields to the bundle.'),
+      '#default_value' => empty($this->configuration['source_field']) ? NULL : $this->configuration['source_field'],
+      '#options' => $options,
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function attachConstraints(MediaInterface $media) {
+    parent::attachConstraints($media);
+
+    if (isset($this->configuration['source_field'])) {
+      $source_field_name = $this->configuration['source_field'];
+      if ($media->hasField($source_field_name)) {
+        foreach ($media->get($source_field_name) as &$embed_code) {
+          /** @var \Drupal\Core\TypedData\DataDefinitionInterface $typed_data */
+          $typed_data = $embed_code->getDataDefinition();
+          $typed_data->addConstraint('GoogleDocsEmbedCode');
+        }
+      }
+    }
+  }
+
+  /**
+   * Runs preg_match on embed code/URL.
+   *
+   * @param MediaInterface $media
+   *   Media object.
+   *
+   * @return array|bool
+   *   Array of preg matches or FALSE if no match.
+   *
+   * @see preg_match()
+   */
+  protected function matchRegexp(MediaInterface $media) {
+    $matches = array();
+    if (isset($this->configuration['source_field'])) {
+      $source_field = $this->configuration['source_field'];
+      if ($media->hasField($source_field)) {
+        $property_name = $media->{$source_field}->first()->mainPropertyName();
+        foreach (static::$validationRegexp as $pattern => $key) {
+          if (preg_match($pattern, $media->{$source_field}->{$property_name}, $matches)) {
+            return $matches;
+          }
+        }
+      }
+    }
+    return FALSE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDefaultThumbnail() {
+    return $this->config->get('icon_base') . '/googledocs.png';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function thumbnail(MediaInterface $media) {
+    if ($local_image = $this->getField($media, 'thumbnail_local')) {
+      return $local_image;
+    }
+
+    return $this->getDefaultThumbnail();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDefaultName(MediaInterface $media) {
+    // Try to get some fields that need the API, if not available, just use the
+    // shortcode as default name.
+
+    $username = $this->getField($media, 'username');
+    $id = $this->getField($media, 'id');
+    if ($username && $id) {
+      return $username . ' - ' . $id;
+    }
+    else {
+      $code = $this->getField($media, 'shortcode');
+      if (!empty($code)) {
+        return $code;
+      }
+    }
+
+    return parent::getDefaultName($media);
+  }
+
+}
